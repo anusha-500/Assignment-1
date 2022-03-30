159ed9a9f0f8a823b4708c177b6f8f5ef14e87cf
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-03-30 15:47:52 +0530

    Add files via upload

---
+++ q2.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<h2>This is for Question 2</h2>
+<p>Using V-for</p>
+<p v-for="tabledata in table_data" :key="tabledata.id">
+{{ tabledata.text }}
+</p>
+</div>
+</template>
+<script>
+export default {
+name: "Que2A",
+data() {
+return {
+table_data: [
+{ text: "Name : Agile CRM" },
+{ text: "description: A Company of Excellence" },
+{ text: "years of experience: 12" },
+{ text: "location:Hyderabad" },
+],
+};
+},
+};
+</script>
+
+
