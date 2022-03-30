59c003ac87f08beaf8d1710f265191ac0b6f53ff
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-03-30 15:48:54 +0530

    Add files via upload

---
+++ q5.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<h2>name:{{name}}</h2>
+<input type=text placeholder="enter the text" @change='$emit("userdetails",$event.target.value)'>
+</div>
+</template>
+<script>
+export default{
+name:"SuKanya",
+//props:{
+//name:String
+//}
+methods:{
+userdetails(event){
+this.$emit("Anusha",event.target.value)
+}
+}
+}
+</script>
