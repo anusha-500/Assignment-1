40c3aa0d5a776edb40f22b55f03df29778bc5203
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-03-30 15:48:16 +0530

    Add files via upload

---
+++ q3.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Question 3</h2>
+        <p>This is for components Registration local and global</p>
+    </div>
+</template>
+<script>
+export default{
+    name:'Que3A',
+    data(){
+        return{
+           msg: "Today we have an assignment",
+
+        }
+    }
+    
+}
+</script>
