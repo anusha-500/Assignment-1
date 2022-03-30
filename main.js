ff61ff5aeeffc1f4ed3b1cc975197c4f7c13cd71
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-03-30 15:57:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue } from "bootstrap-vue";
+import { IconsPlugin } from 'bootstrap-vue'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+let vueRoute=new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
+export default vueRoute
