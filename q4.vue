9a1d8186415d80fa54a3abd4312400f3f4cb74ec
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-03-30 15:48:33 +0530

    Add files via upload

---
+++ q4.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id=demo>
+       <p>name: {{name}}</p>
+       <p> mailid:{{mailid}}</p>
+       <p>password:{{password}}</p>
+       <p>phone no:{{number}}</p>
+       <p>Gender:{{gender}}</p>
+        </div>
+</template>
+<script>
+export default{
+    name:'HelloWord',
+    props:{
+        name:String,
+        mailid:String,
+        password:String,
+        number:{default:"1234567890"},
+        gender:{default:"female"},
+
+    },
+}
+</script>
